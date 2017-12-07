36e38cb48c78928feade62f5b1aa404f6a3e16b0
Alekhya90 <kandikantialekhya@gmail.com>
2017-12-06 21:07:36 -0800

    test

---
+++ src/Osclogin.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class Osclogin {
+		@Test
+		public void test(){
+		int a =2;
+		int b =4;
+		int c =a+b;
+		System.out.println("Sum is:" +c);		
+			}
+}
+
